fc032511d8732da8c6ed657e90fd861b325b23fb
Santi Pellegrino <pellegrino.santi@gmail.com>
2015-11-25 14:52:02 -0300

    Fin pagina ayuda
    Quedo como la pagina de homero jajaja

--- src/grafica/PantallaAyuda.java
+++ src/grafica/PantallaAyuda.java
@@ -67,6 +67,26 @@ public class PantallaAyuda extends JFrame {
 		label_1.setBounds(16, 17, 52, 52);
 		contentPane.add(label_1);
 		
+		JLabel label_3 = new JLabel("");
+		label_3.setIcon(new ImageIcon(PantallaAyuda.class.getResource("/res/pajaro3gif.gif")));
+		label_3.setBounds(423, 167, 72, 52);
+		contentPane.add(label_3);
+		
+		JLabel label_6 = new JLabel("");
+		label_6.setIcon(new ImageIcon(PantallaAyuda.class.getResource("/res/teclas.png")));
+		label_6.setBounds(72, 176, 85, 72);
+		contentPane.add(label_6);
+		
+		JLabel label_5 = new JLabel("");
+		label_5.setIcon(new ImageIcon(PantallaAyuda.class.getResource("/res/spacegif.gif")));
+		label_5.setBounds(162, 69, 102, 89);
+		contentPane.add(label_5);
+		
+		JLabel label_4 = new JLabel("");
+		label_4.setIcon(new ImageIcon(PantallaAyuda.class.getResource("/res/ladrillogif.gif")));
+		label_4.setBounds(507, 146, 46, 102);
+		contentPane.add(label_4);
+		
 		JLabel label_2 = new JLabel("");
 		label_2.setIcon(new ImageIcon(PantallaAyuda.class.getResource("/res/pastel2.gif")));
 		label_2.setBounds(237, 336, 61, 52);
